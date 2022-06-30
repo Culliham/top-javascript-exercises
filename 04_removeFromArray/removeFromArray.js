d93b6dc3fc6aafdaf5cee251ae3e66329b0ee44e
Ryan <Ryan.Cullinan.37@gmail.com>
2022-06-30 21:07:47 +0930

    parse multiple args. filter array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+var removeFromArray = function() {
+    //accept any number of arguments
+    let args = Array.from(arguments);
+    //console.log(args);
 
-};
+    let valuesArray = args[0];
+    //console.log(valuesArray);
+
+    let filterArray = args.slice(1);
+    //console.log(filterArray);
+
+    let filteredArray = valuesArray.filter(value => !filterArray.includes(value));
+    //console.log(filteredArray);
+ 
+    return filteredArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
