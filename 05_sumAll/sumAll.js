0606a04a23271aa1587157e7858c4a4c815545e7
Ryan <Ryan.Cullinan.37@gmail.com>
2022-06-30 21:22:14 +0930

    check inputs. find smaller and larger. sum.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let isNumbers = (typeof a == 'number' && typeof b == 'number');
+    let isNonNegatives = (a>0 && b>0);
 
+    let [smaller, greater] = a<b ? [a, b]: [b, a]; 
+
+    let sum = 0;
+    if (isNumbers && isNonNegatives) {
+        for (let i=smaller; i<=greater; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    
+    return "ERROR";
 };
 
 // Do not edit below this line
