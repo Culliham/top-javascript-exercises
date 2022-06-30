37602a08e237419e662c2f10f0d7d7ed6ab4ad87
Ryan <Ryan.Cullinan.37@gmail.com>
2022-06-30 20:33:30 +0930

    add repeatString. catch negative num.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    var resultString = "";
+    if (num<0) {
+        resultString = "ERROR";
+    } else {
+        for (let i=0; i<num; i++) {
+            resultString += str;
+        }
+    }
+    return resultString;
 };
 
 // Do not edit below this line
