964a85e2e8aa1a922462d1bed8f8d5b2fa3d4c81
Ryan <Ryan.Cullinan.37@gmail.com>
2022-08-02 21:17:37 +0930

    add functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(numA, numB) {
+	return numA + numB;
 };
 
-const subtract = function() {
-	
+const subtract = function(numA, numB) {
+	return numA - numB;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((partialSum, a) => partialSum + a, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((partialProduct, a) => partialProduct*a, 1);
 };
 
-const power = function() {
-	
+const power = function(numA, numB) {
+	return numA ** numB;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if ((num==1) || (num==0)) {
+    return 1;
+  } else {
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
