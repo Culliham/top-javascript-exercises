44329e9ce52611f58bc6a13cb00ea943906cdb45
Ryan <Ryan.Cullinan.37@gmail.com>
2022-08-02 21:44:11 +0930

    add palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    var indexLeft = 0;
+    var indexRight = str.length - 1;
 
+    var isPalindrome = true;
+
+    while (indexLeft < indexRight) {
+        if (!isLetter(str.at(indexLeft))) {
+            ++indexLeft;
+        } else if (!isLetter(str.at(indexRight))) {
+            --indexRight;
+        } else {
+            if (str.at(indexLeft) != str.at(indexRight)) {
+                isPalindrome = false;
+            }
+            indexLeft++;
+            indexRight--;
+        }
+    }
+    return isPalindrome;
+};
+
+const isLetter = function (char) {
+    return char.length===1 && char.match(/[a-z]/i);
 };
 
 // Do not edit below this line
